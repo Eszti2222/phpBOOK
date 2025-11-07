7e8799fe7f326e6b157524cff4da538f46bb3a6d
Eszti2222 <nagy.eszter2222@gmail.com>
2025-11-07 16:47:08 +0100

    javitott token

--- library_project/routes/api.php
+++ library_project/routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+Route::middleware(['auth:sanctum'])
+->group(function () {
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    // Kijelentkezés útvonal
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 });
